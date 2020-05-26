123deb725d1901e47cd9db7a3c4f85767fac8f06
Gaston Alejandro <Gaston Alejandro@LAPTOP-6VUQESGH>
2020-05-26 11:35:31 +0200

    Prueba GitHub Gastón Fabara.

---
+++ PruebaGit/src/prueba/Edad.java
@@ -0,0 +1,39 @@
+package prueba;
+import java.util.Scanner;
+
+public class Edad {
+
+
+		private static Scanner entrada;
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			entrada = new Scanner(System.in);
+			
+			System.out.println("Introduce tu edad, por favor");
+			
+			int edad=entrada.nextInt();
+			
+			if (edad<18) {
+				System.out.println("Eres un adolescente");
+			}
+			
+			else if(edad<40) {
+				System.out.println("Eres joven");
+			}
+			
+			else if(edad<65) {
+				
+				System.out.println("Eres maduro");
+			}
+			
+			else {
+				
+				System.out.println("Cuidate");
+			}
+	}
+			
+	}
+
+
